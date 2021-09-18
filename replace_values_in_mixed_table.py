fd9cdde7decd1cc6cfb08cb652314fd2db429133
dreamwork-Barel-and-Yuval <dreamwork@dreamwork.network>
2021-09-18 07:03:44 +0300

    get it to work

--- replace_values_in_mixed_table.py
+++ replace_values_in_mixed_table.py
@@ -1,17 +1,15 @@
 import numpy as np
 import pandas as pd
 
-def create_table(n_fetuers=10, ids=4, n_reapte_ids=4):
-  rows = ids * n_reapte_ids
-  ids = np.tile([id_ for id_ in range(1, n_reapte_ids+1)], n_reapte_ids)
-  values = np.random.randint(1, 1000, size=(rows, n_fetuers))
-  fetuers = [f'feature_{n}' for n in range(n_fetuers)]
-  data = pd.DataFrame(values, columns=fetuers)
-  ids_series = pd.Series(ids, name="IDs")
-  return pd.concat([ids_series, data], axis=1)
 
+def create_table(n_fetuers=10, ids=4, n_reapte_ids=4):
+    rows = ids * n_reapte_ids
+    ids = np.tile([id_ for id_ in range(1, n_reapte_ids+1)], n_reapte_ids)
+    values = np.random.randint(1, 1000, size=(rows, n_fetuers))
+    fetuers = [f'feature_{n}' for n in range(n_fetuers)]
+    data = pd.DataFrame(values, columns=fetuers)
+    ids_series = pd.Series(ids, name="IDs")
+    return pd.concat([ids_series, data], axis=1)
 
 
 print(create_table())
-
-
